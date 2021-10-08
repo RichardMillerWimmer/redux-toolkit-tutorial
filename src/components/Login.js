08f2fa511cc611a29194f972651b901074b6237b
Richard Miller <richardmmill@gmail.com>
2021-10-07 21:32:28 -0400

    logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { login, logout } from '../features/user';
 
 const Login = () => {
+    const dispatch = useDispatch();
+
     return (
         <div>
-            <button>Login</button>
+            <button
+                onClick={() => dispatch(login({ name: 'Rich', age: 33, email: 'rich@email.com' }))}
+            >Login</button>
+            <button onClick={() => dispatch(logout())}>Logout</button>
         </div>
     )
 }
